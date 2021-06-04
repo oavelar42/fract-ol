82ffc225c4383aa8c9d49caf7d732996c53cb0a4
otavio <oavelar@student.42lisboa.com>
2021-06-04 14:37:20 +0100

    init main fractol...

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,15 +6,47 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 19:01:36 by oavelar           #+#    #+#             */
-/*   Updated: 2021/05/31 19:03:09 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/06/04 14:26:37 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
-#include "libft.h"
+# include "mlx.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <math.h>
+# include <float.h>
+# include <fcntl.h>
+# include "libft.h"
 
+# if defined(__linux__) || defined(__unix__)
+#  define ESC 65307
+#  define CLOSE 33
 
+# elif __APPLE__
+#  define ESC 53
+#  define CLOSE 17
+
+#endif
+
+typedef struct  s_image
+{
+    void    *image;
+}               t_image;
+
+typedef struct  s_fractal
+{
+    char    *name;
+}               t_fractal;
+
+typedef struct s_mlx
+{
+    void    *mlx;
+    void    *window;
+    t_image *image;
+}               t_mlx;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,9 +6,56 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 16:51:43 by oavelar           #+#    #+#             */
-/*   Updated: 2021/05/31 19:01:22 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/06/04 14:36:37 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+int	close_program(t_mlx *mlx)
+{
+	mlx_destroy_window(mlx->mlx, mlx->window);
+	scene_quit(mlx);
+	exit(EXIT_SUCCESS);
+	return (0);
+}
+
+int	key_hook(int keycode, t_mlx *mlx)
+{
+	if (keycode == ESC)
+	{
+		printf("\n\nClosed with ESC\n");
+		close_program(mlx);
+	}
+	else if (mlx == 1)
+		push_image(mlx);
+	return (1);
+}
+
+void	push_image(t_mlx *mlx)
+{
+	render(mlx);
+	mlx_clear_window(mlx->mlx, mlx->window);
+	mlx_put_image_to_window(mlx->mlx, mlx->window, mlx->image, 0, 0);
+}
+
+int	main(int ac, char **av)
+{
+	t_mlx		*mlx;
+	t_fractal	*f;
+
+	if (ac < 2)
+		printf("\nERROR : not enough arguments\n");
+	f = fractal_match(av[1]);
+	if (f->name == NULL)
+		printf("\nInvalid fractal name\n");
+	if ((mlx = init(f)) == NULL)
+		printf("\nMLX couldn't init\n");
+	reset_vierport(mlx);
+	render(mlx);
+	mlx_hook(mlx->window, CLOSE, 0L, close_program, mlx);
+	mlx_key_hook(mlx->window, key_hook, mlx);
+	mlx_expose_hook(mlx->window, push_image, mlx);
+	mlx_loop(mlx->mlx);
+	return (0);
+}
